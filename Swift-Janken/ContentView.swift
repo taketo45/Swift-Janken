688182995f592e1fa6b37c3625a1488af55566fa
“taketo45” <taketo45@gmail.com>
2025-01-24 19:52:22 +0900

    Initial Commit

---
+++ Swift-Janken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift-Janken
+//
+//  Created by Taketoshi Ikegai on 2025/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift-Janken/Swift_JankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_JankenApp.swift
+//  Swift-Janken
+//
+//  Created by Taketoshi Ikegai on 2025/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_JankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
