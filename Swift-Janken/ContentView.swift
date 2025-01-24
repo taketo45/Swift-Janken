ea1b9aff03f0bcfb02355877c23f2025b4b4a267
“taketo45” <taketo45@gmail.com>
2025-01-24 23:50:21 +0900

    Refacter JudgeHand Function & variable name of Image

--- Swift-Janken/ContentView.swift
+++ Swift-Janken/ContentView.swift
@@ -8,17 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var imageName = "janken"
+    @State var winlose = ""
+    @State var playerHandImage = "janken"
+    @State var pcHandImage = "janken"
+    @State var playerHand = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(imageName)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            HStack {
+                Image(playerHandImage)
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Image(pcHandImage)
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+            }
+            Text(winlose)
         }
         .padding()
+        HStack {
+            Button("ぐー"){
+                junken("gu")
+            }
+            .modifier(ButtonModifier(backgtoundColor: .red))
+            Button("ちょき"){
+                junken("choki")
+            }
+            .modifier(ButtonModifier(backgtoundColor: .blue))
+            Button("ぱー"){
+                junken("pa")
+            }
+            .modifier(ButtonModifier(backgtoundColor: .green))
+        }
+
     }
+    
+    func junken(_ playerHand: String){
+        playerHandImage = playerHand
+        let pcHand = getPcHand()
+        pcHandImage = pcHand
+        winlose = JudgeHand(playerHand, pcHand)
+    }
+    
 }
 
 #Preview {
     ContentView()
+}
+
+struct ButtonModifier: ViewModifier {
+    let backgtoundColor: Color
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity, minHeight: 50)
+            .background(backgtoundColor)
+            .foregroundColor(.white)
+    }
+}
+
+func getPcHand()->String {
+    let dashites = ["gu", "choki", "pa"]
+    
+    return dashites[Int.random(in: 0...2)]
+}
+
+func JudgeHand(_ playerHand:String, _ pcHand:String)->String{
+    let results: [String: [String: String]] = [
+        "gu": ["pa": "まけ", "choki": "かち"],
+        "choki": ["gu": "まけ", "pa": "かち"],
+        "pa": ["choki": "まけ", "gu": "かち"]
+    ]
+    return playerHand == pcHand ? "あいこ" : (results[playerHand]?[pcHand] ?? "")
+    
 }
